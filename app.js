5108154f3809f8a9fc4f30ba07e4994b2d105ef8
Sergiusz R <sergiuszrozycki@icloud.com>
2024-02-18 14:40:40 +0100

    dynamic update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let globalBillingDetails = null;
 let globalDetails = null;
+let globalEmail = null;
 let geoToken =
   "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkVzROZW9TeXk0OHpCOHg4emdZX2t5dFNiWHY3blZ0eFVGVFpzWV9TUFA4In0.eyJleHAiOjIwMjM0MzYxNDQsImlhdCI6MTcwODA3NjE0NCwianRpIjoiNWFlOGU2NGQtMzM5YS00ZDZiLThjMTUtMzJiZTMyNzliZmVlIiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWxvZ2luLmlucG9zdC5wbC9hdXRoL3JlYWxtcy9leHRlcm5hbCIsInN1YiI6ImY6N2ZiZjQxYmEtYTEzZC00MGQzLTk1ZjYtOThhMmIxYmFlNjdiOjFBNlZvZGcwdGFvLVYxa1RXV1dFSzBRdlFGQWlMbGhyYS1HdVJwVkVNSEUiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJzaGlweCIsInNlc3Npb25fc3RhdGUiOiIwNDdiN2ExMC02ZGY3LTQ1OWUtYmY5Ni04N2YzZjFmMThjZjkiLCJzY29wZSI6Im9wZW5pZCBhcGk6YXBpcG9pbnRzIiwic2lkIjoiMDQ3YjdhMTAtNmRmNy00NTllLWJmOTYtODdmM2YxZjE4Y2Y5IiwiYWxsb3dlZF9yZWZlcnJlcnMiOiJzbmlwY2FydC10ZXN0b3d5LndlYmZsb3cuaW8iLCJ1dWlkIjoiZDc2YmRjNWUtMDU0NS00OThhLTg2MjctNjRkOWZlMTA5NjlmIn0.JrAU0bV3kDpuZPIe-UkkJRxDdaVJrNayqtQ4saMeUO9eHY4MnQyqKM-_ori7HYYKE0Z0tw9aMANgBWKCf_RMJQM7eTHSmgSkLGx96qbyxDIY7CPMpmnx0rnJuN6lfi142Bwe45MYshdjvAc3I9eFTLzkSTve_mhjYhsoq787Tjo941-KLpQUM2OFMgihv8u9pJ2QIAx8YPogk5TqhyrcbehsK7N-IHuiKlFUlSY3OQ1ockuteItIlCboHwZde1aODZcJgqfe-aS4KmNz-nHNkn3jPbPtJ0ODVoyb0UZEZpU-8nIJtqXFIGf2OcdwX875TGDIj3VMzKD96JV2_G4P7Q";
 let apiToken =
@@ -135,31 +136,31 @@ $(document).ready(function () {
     geowidget.addEventListener("handlePointSelection", async (event) => {
       globalDetails = event.detail;
       globalBillingDetails = Snipcart.store.getState().cart.billingAddress;
-      console.log(globalBillingDetails);
+      globalEmail = Snipcart.store.getState().cart.email;
       try {
         await Snipcart.api.cart.update({
-          email: "john.doe@snipcart.com",
+          email: globalEmail,
           shipToBillingAddress: false,
           billingAddress: {
-            name: globalBillingDetails.fullName,
-            firstName: globalBillingDetails.fullName,
+            name: globalBillingDetails.name,
+            firstName: globalBillingDetails.firstName,
             address1: globalBillingDetails.address1,
             city: globalBillingDetails.city,
             country: globalBillingDetails.country,
             postalCode: globalBillingDetails.postalCode,
             province: globalBillingDetails.province,
-            phone: "435123980",
+            phone: globalBillingDetails.phone,
           },
           shippingAddress: {
-            name: globalBillingDetails.fullName,
-            firstName: globalBillingDetails.fullName,
+            name: globalBillingDetails.name,
+            firstName: globalBillingDetails.firstName,
             address1: globalDetails.address.line1,
             address2: `${globalDetails.name}, ${globalDetails.location_description}`,
             city: globalDetails.address_details.city,
             country: globalBillingDetails.country,
             postalCode: globalDetails.address_details.post_code,
             province: globalDetails.address_details.province,
-            phone: "435123980",
+            phone: globalBillingDetails.phone,
           },
         });
 
@@ -182,15 +183,15 @@ $(document).ready(function () {
 function applyGeowidgetStyles() {
   var style = document.createElement("style");
   style.innerHTML = `
-  .custom-geowidget {
-    height: 35vh;
-    margin-bottom: 4rem;
-  }
-
-  .custom-geowidget #geowidget {
-    height: 100%;
-  }
-`;
+      .custom-geowidget {
+        height: 35vh;
+        margin-bottom: 4rem;
+      }
+    
+      .custom-geowidget #geowidget {
+        height: 100%;
+      }
+    `;
   document.head.appendChild(style);
 }
 document.addEventListener("snipcart.ready", function () {
@@ -207,10 +208,10 @@ document.addEventListener("snipcart.ready", function () {
       },
       data: JSON.stringify({
         receiver: {
-          first_name: globalBillingDetails.fullName,
-          last_name: globalBillingDetails.fullName,
-          email: "test@inpost.pl",
-          phone: "435123980",
+          first_name: globalBillingDetails.firstName,
+          last_name: globalBillingDetails.name,
+          email: globalEmail,
+          phone: globalBillingDetails.phone,
         },
         parcels: {
           template: "small",
@@ -224,14 +225,15 @@ document.addEventListener("snipcart.ready", function () {
           currency: "PLN",
         },
         custom_attributes: {
-          sending_method: "dispatch_order",
+          sending_method: "parcel_locker",
           target_point: globalDetails.name,
         },
         service: "inpost_locker_standard",
-        reference: "Test",
+        reference: "snipcart",
       }),
       success: function (response) {
         console.log("Server response:", response);
+        // Shipment created successfully; you can now handle it in your package manager
       },
       error: function (error) {
         console.error("Error creating shipment:", error);
